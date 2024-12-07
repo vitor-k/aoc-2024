23bf9fec1490ba93f28215ee77dd6513b87b2517
Vitor Kiguchi <vitor-kiguchi@hotmail.com>
2024-12-07 16:14:06 -0300

    day07: timing and small optimizations

--- day07/main.py
+++ day07/main.py
@@ -1,17 +1,19 @@
 
 from itertools import product
+import time
 
-
-def applyOperators(operands, operators):
+def applyOperators(operands, operators, expected_result):
     result = operands[0]
     for operand, operator in zip(operands[1:], operators):
-        match operator:
-            case '+':
-                result += operand
-            case '*':
-                result *= operand
-            case '||':
-                result = int(str(result) + str(operand))
+        if operator == '+':
+            result += operand
+        elif operator == '*':
+            result *= operand
+        elif operator == '||':
+            result = int(f"{result}{operand}")
+
+        if result > expected_result: # early abort
+            break
     return result
 
 def part1(filename):
@@ -21,9 +23,9 @@ def part1(filename):
         for line in fp.readlines():
             result, operands = line.strip().split(':')
             result = int(result)
-            operands = list(map(int, operands.split()))
-            for operators in product(['+', '*'], repeat=len(operands)-1):
-                possible_result = applyOperators(operands, operators)
+            operands = tuple(map(int, operands.split()))
+            for operators in product(('+', '*'), repeat=len(operands)-1):
+                possible_result = applyOperators(operands, operators, result)
                 if possible_result == result:
                     total += result
                     break
@@ -36,9 +38,9 @@ def part2(filename):
         for line in fp.readlines():
             result, operands = line.strip().split(':')
             result = int(result)
-            operands = list(map(int, operands.split()))
-            for operators in product(['+', '*', '||'], repeat=len(operands)-1):
-                possible_result = applyOperators(operands, operators)
+            operands = tuple(map(int, operands.split()))
+            for operators in product(('+', '*', '||'), repeat=len(operands)-1):
+                possible_result = applyOperators(operands, operators, result)
                 if possible_result == result:
                     total += result
                     break
@@ -48,5 +50,10 @@ def part2(filename):
 if __name__ == "__main__":
     filename = "example.txt"
     filename = "input.txt"
+    t0 = time.time_ns()
     part1(filename)
+    t1 = time.time_ns()
     part2(filename)
+    t2 = time.time_ns()
+
+    print(f"Part 1: {(t1-t0)/1e9} s\nPart 2: {(t2-t1)/1e9} s")
